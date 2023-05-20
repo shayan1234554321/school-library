1055975950a993a78a27ebe9ea039de4fa8096de
shayan1234554321 <kshayan091@gmail.com>
2023-05-21 01:48:40 +0500

    Added UI

---
+++ student.rb
@@ -0,0 +1,19 @@
+require_relative 'classroom'
+
+class Student < Person
+  attr_accessor :classroom
+
+  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    '¯\\(ツ)/¯'
+  end
+
+  def classrooms=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
+  end
+end
