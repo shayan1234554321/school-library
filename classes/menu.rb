5899b827bdd30c89397283a34838b70609ed8fc3
iqbal-elham <iqbal.elham7@gmail.com>
2023-05-24 17:40:56 +0430

    Fix the linter error

--- classes/menu.rb
+++ classes/menu.rb
@@ -2,7 +2,7 @@ require_relative '../app'
 
 class Menu
   def initialize(books, people, rentals)
-    @APP = App.new(books, people, rentals)
+    @app = App.new(books, people, rentals)
   end
 
   def print_menu
@@ -19,17 +19,17 @@ class Menu
   def result(user_choice)
     case user_choice
     when 1
-      @APP.list_books
+      @app.list_books
     when 2
-      @APP.list_people
+      @app.list_people
     when 3
-      @APP.create_person
+      @app.create_person
     when 4
-      @APP.create_book
+      @app.create_book
     when 5
-      @APP.create_rental
+      @app.create_rental
     when 6
-      @APP.list_rentals_by_person_id
+      @app.list_rentals_by_person_id
     end
   end
 end
