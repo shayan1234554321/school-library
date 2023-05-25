a01dbe07523be0d70fd766c5fa0c22d2ab6d8976
shayan1234554321 <kshayan091@gmail.com>
2023-05-25 20:58:03 +0500

    classroom testing

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,8 +1,12 @@
 require_relative '../classes/book'
+require_relative '../classes/rental'
+require_relative '../classes/person'
 
-describe "Book" do
+describe Book do
     context "Create instance of Book class" do
         book = Book.new('Ruby', 'Shayan')
+        person = Person.new("Student", 22)
+        date = "2019"
         it "should return instance of Book class" do
             expect(book).to be_instance_of(Book)
         end
@@ -11,6 +15,11 @@ describe "Book" do
         end
         it "returns the correct author" do
             book.author.should eql "Shayan"
+        end
+        it "Adding rentals" do
+            # print person.type
+            # book.add_rentals(person , date)
+            # expect(book.rentals).to include(person)
         end
     end
 end
